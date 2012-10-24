ebba46d55b901c21ea3a689a18fac59d48611c51
josue <josss9229@gmail.com>
2012-10-24 21:47:46 +0100

    Tirada humano

--- lib/clase_ppt.rb
+++ lib/clase_ppt.rb
@@ -1,3 +1,10 @@
-class Ppt
 
+class Ppt
+  attr_accessor  :jugada_j
+  attr_reader :jugada_j
+
+   def initialize
+
+	@jugada_j= :piedra  
+   end 
 end
--- spec/spec_ppt.rb
+++ spec/spec_ppt.rb
@@ -1,4 +1,14 @@
-require "~/LPP/Pract6/lib/clase_ppt"
+require "~/LPP/Pract6/lib/clase_ppt" 
+
+describe Ppt do
+  before :each do
+	  @mano = Ppt.new
+  end
+  it "Deberia tener un estado inicial el jugador" do
+	@mano.jugada_j.should == :piedra   
+  end 
+
+end
 
 
 
